bb73d8915c303b592aa2c74f0c812ea63a6adad9
Leonardo <leoalves226x@gmail.com>
2025-09-01 02:23:14 -0300

    E2e

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    testIsolation: false,
+    testIsolation: true,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
